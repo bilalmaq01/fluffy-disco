968ba808679769a2d452645488b7e9b64bfc0669
AAlexander323 <abralexander323@gmail.com>
2025-10-09 17:54:06 -0500

    Update Main.java
    Added the previous print statements, and moved all previously existing code in the conversation to prevent an error statement

--- Main.java
+++ Main.java
@@ -2,6 +2,13 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.print("Hello, Hunter");
+        System.out.print("Hello Gavin");
+        System.out.print("12345");
+        System.out.print("54321");
+
         // Declare values
         Scanner scanner = new Scanner(System.in);
         String input;
