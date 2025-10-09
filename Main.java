1eb9607b7fe669c868ff65c77af0f4b9b07721ef
gbplaunt22 <58701655+gbplaunt22@users.noreply.github.com>
2025-10-09 18:21:01 -0400

    Added console print test
    Added a line that prints "Hello Gavin" To see if my commit goes through!

--- Main.java
+++ Main.java
@@ -5,6 +5,6 @@ public class Main {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.print("Hello, Hunter");
-
+        System.out.print("Hello Gavin");
     }
 }
