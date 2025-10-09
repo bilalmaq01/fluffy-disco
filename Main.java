671305fc227074631031be5855d8382850b570d8
AAlexander323 <abralexander323@gmail.com>
2025-10-09 17:12:16 -0500

    Update Main.java
    Creates rudimentary dialogue that prompts the user with four options: "fluff, disco, fluffy disco," or "neither." 
    
    If the input matches either of the first three options, regardless of capitalization, the program responds with the appropriate comment. Otherwise, the program responds with a default option.
    
    Once the program has completed the small interaction, it closes the scanner and ends the program.

--- Main.java
+++ Main.java
@@ -1,10 +1,32 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.print("Hello, Alex!");
+        // Declare values
+        Scanner scanner = new Scanner(System.in);
+        String input;
 
+        // Opening conversation
+        System.out.println("Welcome to the Fluffy Disco.");
+        System.out.println("Are you here for the fluff or the disco?");
+
+        // All options & Input
+        System.out.printf("\n Fluff \t \t \t \t Disco \n Fluffy Disco \t \t Neither \n \n > ");
+        input = scanner.next();
+
+        // If-else statement 
+        // (Three options - if it doesn't match any of the three options, chooses a default response)
+        if (input.equalsIgnoreCase("fluffy")) {
+            System.out.println("\n Of course, it's so fluffy!");
+        } else if (input.equalsIgnoreCase("disco")) {
+            System.out.println("\n Sounds like a good time.");
+        } else if (input.equalsIgnoreCase("fluffy disco")) {
+            System.out.println("\n Yes! Best of both worlds!");
+        } else {
+            System.out.println("\n... then why are you here?");
+        }
+        
+        // End program - close scanner
+        scanner.close();
     }
 }
